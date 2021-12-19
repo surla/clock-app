c6eaa50b2686b83593952e9db6f1efe8347a1c0e
Joseph Surla <josephsurla@gmail.com>
2021-12-18 20:53:24 -0800

    add google font Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        "mobile-daytime": "url('public/assets/mobile/bg-image-daytime.jpg')",
+        "mobile-nighttime":
+          "url('public/assets/mobile/bg-image-nighttime.jpg')",
+      },
+    },
   },
   plugins: [],
 };
